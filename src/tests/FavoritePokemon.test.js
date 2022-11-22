30547d994415c854c29645ce2ff6c0089d71b9f1
Matheus Biaggio <matheusbiaggio@gmail.br>
2022-11-22 19:17:05 -0300

    termino requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,30 @@
-test('', () => {});
+import React from 'react';
+import { FavoritePokemon } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+const data = require('../mock/mockData');
+
+jest.mock('../mock/mockData');
+
+describe('Requisito 3 - Teste o componente <FavoritePokemon.js />', () => {
+  test(`Testa se é exibida na tela a mensagem "No favorite pokemon found",
+  caso a pessoa não tenha Pokémon favoritos`, () => {
+    global.fetch = () => {};
+    // Acessar
+    const { getByText } = renderWithRouter(<FavoritePokemon />);
+    const notFoundEl = getByText(/no favorite pokémon found/i);
+    // Agir
+
+    // Aferir
+    expect(notFoundEl).toBeInTheDocument();
+  });
+  test('Testa se são exibidos todos os cards de Pokémon favoritados', () => {
+    // mocando as informações
+    // Acessar
+    // Agir
+
+    // Aferir
+    console.log(data);
+    expect(data.data.name).toBe('Pikachu');
+  });
+});
